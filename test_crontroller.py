f15093dc25df82a59bbe26b20965ccf0d7faaf2c
Bhatti Abdulmannan (bhattabd) <bhattabd@students.zhaw.ch>
2026-01-06 21:59:08 +0100

    Connection testing file yeyy

---
+++ test_crontroller.py
@@ -0,0 +1,29 @@
+import pygame
+import time
+
+pygame.init()
+pygame.joystick.init()
+
+print("Waiting for connection")
+
+while True:
+    # Refresh list of devices
+    pygame.joystick.quit()
+    pygame.joystick.init()
+    
+    count = pygame.joystick.get_count() #gets amount of connected deccvices
+    
+    if count > 0:
+        controller = pygame.joystick.Joystick(0)
+        controller.init()
+        print(f"\n {controller.get_name()}")
+        print("Connected successfullay")
+        
+        try:
+            controller.rumble(1.0, 1.0, 1000) 
+            print("buzzed")
+        except:
+            print("connected but (no energy?)")
+        break
+    
+    time.sleep(1)
